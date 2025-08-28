e9229f493902e8d5b8ed8a13d8ed56e397669891
jungyh <jungyh870918@gmail.com>
2025-08-28 23:42:36 +0900

    버그 픽스

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.externals = config.externals || [];
+    config.externals.push("bufferutil", "utf-8-validate");
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        bufferutil: false,
+        "utf-8-validate": false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
